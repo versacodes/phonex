2b2ea71ba60bd2c7190e3f7ee8695d077aae1127
Franz Amersian <sianfranzsian@gmail.com>
2023-06-27 11:14:22 +0800

    Made new vue project named vue-store-with-atc(name might change) Made 3 components(StoreHeader, ProductList, ProductCard)

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import StoreHeader from './components/StoreHeader.vue'
+import ProductList from './components/ProductList.vue'
+import ProductCard from './components/ProductCard.vue'
+
+
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
